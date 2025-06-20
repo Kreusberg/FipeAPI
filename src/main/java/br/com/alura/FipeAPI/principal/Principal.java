e1846fc8d0527909f2030b04ad42a8920161d586
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2025-06-20 18:27:28 -0300

    get all models from request vehicle type

--- src/main/java/br/com/alura/FipeAPI/principal/Principal.java
+++ src/main/java/br/com/alura/FipeAPI/principal/Principal.java
@@ -5,6 +5,7 @@ import br.com.alura.FipeAPI.service.ConsumeApi;
 import br.com.alura.FipeAPI.service.DataConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
 import java.util.Scanner;
@@ -29,13 +30,25 @@ public class Principal {
 
         var json = consumer.getData(URL + resposta + "/marcas");
 
-//        DadosVeiculo data =  converter.getData(json, DadosVeiculo.class);
-
 //      Get inicial data based on the vehicle type
         List<DadosVeiculo> data = converter.getData(json, new TypeReference<List<DadosVeiculo>>(){});
 
         data.forEach(d -> {
             System.out.println("Cód: " + d.codigo() + " | Descrição: " + d.nome());
+        });
+
+        System.out.println("Informe o código da marca a ser consultada");
+        var codeBrandResponde = scanner.nextLine().toLowerCase();
+
+        var brandJson = consumer.getData(URL + resposta + "/marcas/" + codeBrandResponde + "/modelos");
+
+        JsonNode root = converter.getDataTree(brandJson);
+        JsonNode modelosNode = root.get("modelos");
+
+        List<DadosVeiculo> modelos = converter.getData(modelosNode.toString(), new TypeReference<List<DadosVeiculo>>() {});
+
+        modelos.forEach(m -> {
+            System.out.println("Cód: " + m.codigo() + " | Descrição: " + m.nome());
         });
 
     }
--- src/main/java/br/com/alura/FipeAPI/service/DataConverter.java
+++ src/main/java/br/com/alura/FipeAPI/service/DataConverter.java
@@ -3,6 +3,7 @@ package br.com.alura.FipeAPI.service;
 import br.com.alura.FipeAPI.model.DadosVeiculo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
@@ -28,9 +29,19 @@ public class DataConverter implements IDataConverter {
 
         try {
             return (T) mapper.readValue(json, classe);
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public JsonNode getDataTree(String json) {
+
+        try {
+            return mapper.readTree(json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
